97d07542f66070380de75aaf0b0953be68e3ed4e
Bruno Henrique <bruno@cbkdigital.com.br>
2020-06-17 13:36:41 -0400

    feature: Router to like repository

--- src/app.js
+++ src/app.js
@@ -67,7 +67,23 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const repository = repositories[repositoryIndex];
+
+  repository.likes++;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
